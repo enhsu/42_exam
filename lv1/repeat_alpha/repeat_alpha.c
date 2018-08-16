4c8238c9df2a925a1c311b36d22bdc70805d0400
Cheng-en Hsu <chsu@e1z1r6p24.42.us.org>
2018-08-16 15:34:48 -0700

    repeat_alpha.c

--- lv1/repeat_alpha/repeat_alpha.c
+++ lv1/repeat_alpha/repeat_alpha.c
@@ -1,21 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   repeat_alpha.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chsu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/15 17:44:48 by chsu              #+#    #+#             */
-/*   Updated: 2018/08/15 17:45:00 by chsu             ###   ########.fr       */
+/*   Created: 2018/08/16 15:22:33 by chsu              #+#    #+#             */
+/*   Updated: 2018/08/16 15:32:42 by chsu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+#include <unistd.h>
+
+void	print_word(char c)
+{
+	int	times;
+
+	if (c >= 'a' && c <= 'z')
+		times = c - 'a' + 1;
+	else if (c >= 'A' && c <= 'Z')
+		times = c - 'A' + 1;
+	else
+		times = 1;
+	while (times > 0)
+	{
+		write(1, &c, 1);
+		times--;
+	}
+}
+
+void	ex_putstr(char *s)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (s[i])
+	{
+		print_word(s[i]);
 		i++;
-	return (i);
+	}
+}
+
+int		main(int ac, char **av)
+{
+	if (ac == 2)
+		ex_putstr(av[1]);
+	write(1, "\n", 1);
 }
