7a73cdabb4c9d05fb5c4d262ed068065e5ea53b4
Cheng-en Hsu <chsu@e1z2r10p5.42.us.org>
2018-12-09 14:57:54 -0800

    reverse_bits

--- lv2/reverse_bits/main.c
+++ lv2/reverse_bits/main.c
@@ -1,28 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   reverse_bits.c                                     :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chsu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/09 14:40:48 by chsu              #+#    #+#             */
-/*   Updated: 2018/12/09 14:57:36 by chsu             ###   ########.fr       */
+/*   Created: 2018/12/09 14:54:44 by chsu              #+#    #+#             */
+/*   Updated: 2018/12/09 14:56:11 by chsu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned char	reverse_bits(unsigned char octet)
-{
-	unsigned char	ret;
-	int				i;
+#include <unistd.h>
 
-	ret = 0;
-	i = 0;
-	while (i < 8)
-	{
-		// (octet & 1) get the rightest one bit
-		ret = (ret << 1) | (octet & 1);
-		octet = (octet >> 1);
-		i++;
-	}
-	return (ret);
+unsigned char	reverse_bits(unsigned char octet);
+
+int				main(void)
+{
+	unsigned char c;
+
+	// expected: '&' -reverse-> 'd'
+	c = '&';
+	write(1, &c, 1);
+	write(1, "\n", 1);
+	c = reverse_bits(c);
+	write(1, &c, 1);
+	write(1, "\n", 1);
+	return (0);
 }
+
+// Bitwise Operators in C:
+// https://www.tutorialspoint.com/cprogramming/c_bitwise_operators.htm
