632ef1df10c2f86a5d3ee557663ea1cf0a1bee3f
Cheng-en Hsu <chsu@e1z2r10p5.42.us.org>
2018-12-09 23:45:37 -0800

    some lv5

--- lv5/rpn_calc/rpn_calc.c
+++ lv5/rpn_calc/rpn_calc.c
@@ -5,16 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: chsu <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/09 15:45:23 by chsu              #+#    #+#             */
-/*   Updated: 2018/12/09 17:56:45 by chsu             ###   ########.fr       */
+/*   Created: 2018/12/09 22:56:23 by chsu              #+#    #+#             */
+/*   Updated: 2018/12/09 23:35:02 by chsu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// NOT CORRECT, number order should be changed.
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "rpn_calc.h"
 
 int		is_space(char c)
 {
@@ -112,7 +108,35 @@ char	*get_op_arr(char *s)
 	return (ret);
 }
 
-int		*get_num_arr(char *s)
+int		get_fn_i(char *s)
+{
+	int	i;
+	int	j;
+	int	fn_i;
+
+	i = 0;
+	fn_i = 0;
+	while (s[i])
+	{
+		j = i;
+		while (s[j] && !is_space(s[j]))
+			j++;
+		// j is space right now
+		j++;	// j is num right now
+		while (s[j] && !is_space(s[j]))
+			j++;
+		// j is space right now, j + 1 should be op
+		if (is_op(s[j + 1]))
+		{
+			fn_i = i;
+			break ;
+		}
+		i++;
+	}
+	return (fn_i);
+}
+
+int		*get_num_arr(char *s, int *arr_l)
 {
 	int	*ret;
 	int	i;
@@ -121,28 +145,7 @@ int		*get_num_arr(char *s)
 	int	ret_i;
 
 	ret = (int*)malloc(sizeof(int) * ft_strlen(s));
-	i = 0;
-	fn_i = 0;
-	while (s[i])
-	{
-		if (!is_space(s[i]) && (i == 0 || is_space(s[i - 1])))
-		{
-			j = i;
-			while (s[j] && !is_space(s[j]))
-				j++;
-			// j is space right now
-			j++;	// j is num right now
-			while (s[j] && !is_space(s[j]))
-				j++;
-			// j is space right now, j + 1 should be op
-			if (is_op(s[j + 1]))
-			{
-				fn_i = i;
-				break ;
-			}
-		}
-		i++;
-	}
+	fn_i = get_fn_i(s);
 	// printf("first index: %i\n", fn_i);
 
 	// from fn_i to the first op
@@ -156,6 +159,7 @@ int		*get_num_arr(char *s)
 			while (s[j] && !is_space(s[j]))
 				j++;
 			ret[ret_i] = get_number(s, i, j);
+			(*arr_l)++;
 			ret_i++;
 		}
 		i++;
@@ -170,10 +174,25 @@ int		*get_num_arr(char *s)
 			while (s[j] && !is_space(s[j]))
 				j++;
 			ret[ret_i] = get_number(s, i, j);
+			(*arr_l)++;
 			ret_i++;
 		}
 		i--;
 	}
+	return (ret);
+}
+
+int		*get_num2_arr(char *s)
+{
+	int	*ret;
+	int	i;
+	int	j;
+	int	fn_i;
+	int	ret_i;
+
+	ret = (int*)malloc(sizeof(int) * ft_strlen(s));
+	fn_i = get_fn_i(s);
+	ret_i = 0;
 	// from fn_i to the end
 	i = fn_i + 1;
 	while (s[i] && !is_space(s[i]))
@@ -196,17 +215,6 @@ int		*get_num_arr(char *s)
 		}
 		i++;
 	}
-	ret[ret_i] = -1;
-	// test
-	/*
-	 int	t = 0;
-	 while (ret[t] != -1)
-	 {
-	 	printf("ret[%i]: %i\n", t, ret[t]);
-	 	t++;
-	 }
-	 */
-	// test
 	return (ret);
 }
 
@@ -232,8 +240,11 @@ void	rpn_calc(char *s)
 {
 	char	*op;
 	int		*num;
+	int		num_l;
+	int		*num2;
 	int		op_i;
 	int		num_i;
+	int		num2_i;
 	int		result;
 
 	if (!input_valid(s))
@@ -241,24 +252,34 @@ void	rpn_calc(char *s)
 		printf("Error");
 		return ;
 	}
+	num_l = 0;
 	op = get_op_arr(s);
-	num = get_num_arr(s);
+	num = get_num_arr(s, &num_l);
+	num2 = get_num2_arr(s);
+	
 	op_i = 0;
 	num_i = 0;
+	num2_i = 0;
 	result = 0;
 	while (op[op_i])
 	{
-		if (num_i == 0)	
+		if (num_i < num_l)
 		{
-			result = do_op(num[num_i], num[num_i + 1], op[op_i]);
-			// printf("result: %i %c %i = %i", num[num_i], op[op_i], num[num_i + 1], result);
-			num_i += 2;
+			if (num_i == 0)
+			{
+				result = do_op(num[num_i], num[num_i + 1], op[op_i]);
+				num_i += 2;
+			}
+			else
+			{
+				result = do_op(num[num_i], result, op[op_i]);
+				num_i++;
+			}
 		}
 		else
 		{
-			result = do_op(result, num[num_i], op[op_i]);
-			// printf("result: %i %c %i = %i", num[num_i], op[op_i], result, result);
-			num_i++;
+			result = do_op(result, num2[num2_i], op[op_i]);
+			num2_i++;
 		}
 		op_i++;
 	}
